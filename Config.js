e7b14c5c039a93bce15be6cdce3615d31b192f2a
AviShek Adhikari <34032131+wisdomrider@users.noreply.github.com>
2019-09-15 00:17:46 +0545

    Delete Config.js

--- Config.js
+++
@@ -1,8 +0,0 @@
-var mongo = {
-    user: "wisdomrider",
-    pass: "nep@li123",
-    useNewUrlParser: true,
-    dbName: "wisdNotes"
-};
-module.exports.mongo = mongo;
-module.exports.config = "mongodb+srv://cluster0-fojqt.mongodb.net/test?retryWrites=true";
